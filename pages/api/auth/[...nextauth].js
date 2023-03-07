a0dce3606ee7d2d2e99ada8849291075f43d068e
ihab nouidjem <ihab.nouidjem01@gmail.com>
2023-03-07 15:14:10 +0100

    nextauth_url added

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,13 +11,13 @@ export default NextAuth({
   ],
   callbacks: {
     // ------------------------------
-    async redirect({ url, baseUrl }) {
-      // Allows relative callback URLs
-      if (url.startsWith("/")) return `${baseUrl}${url}`;
-      // Allows callback URLs on the same origin
-      else if (new URL(url).origin === baseUrl) return url;
-      return baseUrl;
-    },
+    // async redirect({ url, baseUrl }) {
+    //   // Allows relative callback URLs
+    //   if (url.startsWith("/")) return `${baseUrl}${url}`;
+    //   // Allows callback URLs on the same origin
+    //   else if (new URL(url).origin === baseUrl) return url;
+    //   return baseUrl;
+    // },
     // ---------------------------------------
     session: async ({ session, token }) => {
       if (session?.user) {
